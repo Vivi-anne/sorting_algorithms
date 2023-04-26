1e64b6b90373a6a974a64af7ad8d8ad05888639f
Pascal <persiewailer697@gmail.com>
2023-04-26 06:11:30 -0700

    task1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,43 +1,67 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - Sorts a doubly linked list of integers in ascending order
- * @list: Pointer to the head of the doubly linked list
+ * insertion_sort_list - sorts a doubly linked list of integers in ascending order
+ * @list: pointer to the head of the doubly linked list
  */
 void insertion_sort_list(listint_t **list)
 {
+    // Declare pointers to traverse the list
+    listint_t *curr = NULL;
+    listint_t *insertion = NULL;
+
+    // Check for NULL or empty list
     if (list == NULL || *list == NULL || (*list)->next == NULL)
         return;
 
-    listint_t *curr = (*list)->next;
-
+    // Start sorting from the second node
+    curr = (*list)->next;
+    insertion = curr->prev;
     while (curr != NULL)
     {
-        listint_t *insertion = curr->prev;
+        // Store the previous node of the current node
+        insertion = curr->prev;
+        
+        // Move current node to its correct position
         while (insertion != NULL && insertion->n > curr->n)
         {
-            /* Swap nodes */
-            if (insertion->prev != NULL)
-                insertion->prev->next = curr;
-            if (curr->next != NULL)
-                curr->next->prev = insertion;
-
-            insertion->next = curr->next;
-            curr->prev = insertion->prev;
-            curr->next = insertion;
-            insertion->prev = curr;
-
-            /* Update head of list if necessary */
-            if (*list == insertion)
-                *list = curr;
-
-            /* Print current state of list */
-            print_list(*list);
-
+            // Swap current node and the previous node
+            move_left(curr, insertion, list);
             insertion = curr->prev;
         }
-
+        
+        // Move to the next node
         curr = curr->next;
     }
+}
+
+/**
+ * move_left - swaps two adjacent nodes in the doubly linked list
+ * @curr: current node to be moved to the left of insertion
+ * @insertion: insertion pointer
+ * @head: pointer to the head of the doubly linked list
+ */
+void move_left(listint_t *curr, listint_t *insertion, listint_t **head)
+{
+    // Store nodes that need to be swapped
+    listint_t *swap1 = curr->next;
+    listint_t *swap2 = insertion->prev;
+
+    // Update the next and previous nodes of the swapped nodes
+    if (swap1 != NULL)
+        swap1->prev = insertion;
+    if (swap2 != NULL)
+        swap2->next = curr;
+    curr->prev = swap2;
+    insertion->next = swap1;
+    curr->next = insertion;
+    insertion->prev = curr;
+    
+    // Update the head of the list if necessary
+    if (*head == insertion)
+        *head = curr;
+    
+    // Print the current state of the list
+    print_list(*head);
 }
 
