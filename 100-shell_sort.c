5691399756dcb7e5de6ec2c3612f90e9df463021
Pascal <persiewailer697@gmail.com>
2023-04-29 13:28:26 -0700

    task4

---
+++ 100-shell_sort.c
@@ -0,0 +1,42 @@
+#include "sort.h"
+
+/**
+ * shell_sort - sorts an array of integers in ascending order
+ *              using the Shell sort algorithm
+ *
+ * @array: array to be sorted
+ * @size: size of the array
+ */
+void shell_sort(int *array, size_t size)
+{
+    size_t gap, i, j;
+    int temp;
+
+    /* Ensure array exists and has at least two elements */
+    if (array == NULL || size < 2)
+        return;
+
+    /* Start with a big gap, then reduce the gap */
+    for (gap = size / 2; gap > 0; gap /= 2)
+    {
+        /* Do an insertion sort for each gap */
+        for (i = gap; i < size; i++)
+        {
+            /* Save current element to temp */
+            temp = array[i];
+
+            /* Shift elements until the correct position is found */
+            for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
+            {
+                array[j] = array[j - gap];
+            }
+
+            /* Insert the saved element into its correct position */
+            array[j] = temp;
+        }
+
+        /* Print the array after each iteration */
+        print_array(array, size);
+    }
+}
+
