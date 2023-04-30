75b91e052443576666e1fff303f022a689a1997c
Vivi-anne <viviannekwamboka98@gmail.com>
2023-04-29 19:15:53 -0700

    corrections to selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,44 +1,48 @@
 #include "sort.h"
 
 /**
- * selection_sort - sorts an array of integers in ascending order
- * using the selection sort algorithm
+ * swap_ints - Swap two integers in an array.
+ * @i: The first integer to swap.
+ * @j: The second integer to swap.
+ */
+
+void swap_ints(int *i, int *j)
+{
+	int tmp;
+
+	tmp = *i;
+	*i = *j;
+	*j = tmp;
+}
+
+/**
+ * selection_sort - Sort an array of integers in ascending order
+ * @array: An array of integers.
+ * @size: Size of array.
  *
- * @array: pointer to the array to sort
- * @size: size of the array
+ * Description: sorts an array of integers 
+ * 		prints the array after each swap.
+ *
+ * Return: void
  */
 void selection_sort(int *array, size_t size)
 {
-    unsigned long int i, j, smallest_idx;
-    int *temp;
+	size_t a, b; 
+	int *min;
 
-    /* check if array is null or has less than 2 elements */
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    /* iterate over the array */
-    for (i = 0; i < size - 1; i++)
-    {
-        /* assume the current element is the smallest */
-        smallest_idx = i;
+	for (a = 0; a < size - 1; a++)
+	{
+		min = array + a;
+		for (b = a + 1; b < size; b++)
+			min = (array[b] < *min) ? (array + b) : min;
 
-        /* find the smallest element in the unsorted part of the array */
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < array[smallest_idx])
-                smallest_idx = j;
-        }
-
-        /* swap the smallest element with the current element */
-        if (smallest_idx != i)
-        {
-            temp = &array[i];
-            array[i] = array[smallest_idx];
-            array[smallest_idx] = *temp;
-
-            /* print the array after every swap */
-            print_array(array, size);
-        }
-    }
+		if ((array + a) != min)
+		{
+			swap_ints(array + a, min);
+			print_array(array, size);
+		}
+	}
 }
-
